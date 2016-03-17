ec0bdd79d3e6453ab19058a3d51f709422e218ab
Matheus Schuh <mschuh@kalray.eu>
2016-03-17 18:25:39 +0100

    Makefile and mk config files adjustements for current compilation process.

--- misc/mk/common-mppa.mk
+++ misc/mk/common-mppa.mk
@@ -1,2 +1,3 @@
-CFLAGS	= $(CONFIG_MPPA_CC_CFLAGS) -DPOK_ARCH_MPPA -iwithprefix $(KIND_CFLAGS) $(GENERIC_FLAGS) -Wall -g -O0 -Wuninitialized
+CFLAGS = $(CONFIG_MPPA_CC_CFLAGS) -iwithprefix include -fno-builtin -DPOK_ARCH_MPPA $(KIND_CFLAGS) $(GENERIC_FLAGS) -Wall -g -O0 -Wuninitialized
 LDFLAGS += $(CONFIG_MPPA_CC_LFLAGS)
+LDFLAGS += --verbose --print-map
