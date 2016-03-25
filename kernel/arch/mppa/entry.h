46a3c87c9e044d73759bd7ccc7ee21af84e1b698
Matheus Schuh <mschuh@kalray.eu>
2016-03-25 16:54:40 +0100

    End point explicit name and license correction.

--- kernel/arch/mppa/entry.h
+++ kernel/arch/mppa/entry.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2012  University of Padua, Department of Mathematics
+ *  Copyright (C) 2013-2016 Kalray SA.
+ *  All rights reserved.
  *
  *  This file is free software: you may copy, redistribute and/or modify it
  *  under the terms of the GNU General Public License as published by the
@@ -41,13 +42,24 @@
  *		COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
  *		INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
  *		BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *		LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *		LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTEtions and the following
+ *			  disclaimer in the documentation and/or other materials
+ *			  provided with the distribution.
+ *			* Neither the name of the POK Team nor the names of its main
+ *			  author (Julien Delange) or its contributors may be used to
+ *			  endorse or promote products derived from this software
+ *			  without specific prior written permission.
+ *
+ *		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *		AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *		FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NORRUPTION) HOWEVER
  *		CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
  *		LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  *		ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *		POSSIBILITY OF SUCH DAMAGE.
  */
-
+ 
 #ifndef __POK_MPPA_ENTRY_H__
 #define __POK_MPPA_ENTRY_H__
 
