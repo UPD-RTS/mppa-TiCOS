ff18a05c2b63121d42672ca172f8e1adfbb160b3
Matheus Schuh <mschuh@kalray.eu>
2016-06-17 18:15:54 +0200

    User-side libgcc integer routines fixed.

---
+++ libpok/libc/stdio/udivmodsi4.c
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (C) 2013-2016 Kalray SA.
+ *  All rights reserved.
+ *
+ *  This file is free software: you may copy, redistribute and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This file is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ * 		Copyright (c) 2007-2009 POK team
+ *
+ *		Redistribution and use in source and binary forms, with or without
+ *		modification, are permitted provided that the following conditions
+ *		are met:
+ *
+ *			* Redistributions of source code must retain the above copyright
+ *			  notice, this list of conditions and the following disclaimer.
+ *			* Redistributions in binary form must reproduce the above
+ *			  copyright notice, this list of conditions and the following
+ *			  disclaimer in the documentation and/or other materials
+ *			  provided with the distribution.
+ *			* Neither the name of the POK Team nor the names of its main
+ *			  author (Julien Delange) or its contributors may be used to
+ *			  endorse or promote products derived from this software
+ *			  without specific prior written permission.
+ *
+ *		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *		AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *		FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *		COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *		INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *		BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *		LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTEtions and the following
+ *			  disclaimer in the documentation and/or other materials
+ *			  provided with the distribution.
+ *			* Neither the name of the POK Team nor the names of its main
+ *			  author (Julien Delange) or its contributors may be used to
+ *			  endorse or promote products derived from this software
+ *			  without specific prior written permission.
+ *
+ *		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *		AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *		FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NORRUPTION) HOWEVER
+ *		CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *		LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *		ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *		POSSIBILITY OF SUCH DAMAGE.
+ */
+
+static unsigned int udivmodsi4(unsigned int num, unsigned int den, int modwanted)
+{
+    unsigned r = num, q = 0;
+
+    if(den <= r) {
+	unsigned k = __builtin_k1_clz (den) - __builtin_k1_clz (r);
+	den = den << k;
+	if(r >= den) {
+	    r = r - den;
+	    q = 1 << k;
+	}
+	if(k != 0) {
+	    unsigned i = k;
+	    den = den >> 1;
+	    do {
+		r = __builtin_k1_stsu (den, r);
+		i--;
+	    } while (i!= 0);
+	    q = q + r;
+	    r = r >> k;
+	    q = q - (r << k);
+	}
+    }
+
+    return modwanted ? r : q;
+}
+
+unsigned int
+__udivsi3 (unsigned int a, unsigned int b)
+{
+  return udivmodsi4 (a, b, 0);
+}
+
+unsigned int
+__umodsi3 (unsigned int a, unsigned int b)
+{
+  return udivmodsi4 (a, b, 1);
+}
