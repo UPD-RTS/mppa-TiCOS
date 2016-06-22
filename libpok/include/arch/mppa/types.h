8f8a9128a3e90e3529942e5b2de076b04023e4ab
Matheus Schuh <mschuh@kalray.eu>
2016-06-22 15:39:55 +0200

    Redefinition of libpok char type (uint8_t/int8_t).

--- libpok/include/arch/mppa/types.h
+++ libpok/include/arch/mppa/types.h
@@ -59,13 +59,13 @@
 //#include "mOS_constants_c.h"
 //#include "mOS_vcore_c.h"
 
-typedef unsigned short		uint8_t;
+typedef unsigned char		uint8_t;
 typedef unsigned short		uint16_t;
 typedef unsigned int		uint32_t;
 typedef long int		int32_t;
 typedef unsigned long long	uint64_t;
 
-typedef short			int8_t;
+typedef char			int8_t;
 typedef short			int16_t;
 typedef signed long long	int64_t;
 typedef unsigned long long	uint64_t;
