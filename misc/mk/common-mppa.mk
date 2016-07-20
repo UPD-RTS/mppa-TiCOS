91560035f13527e8ccbc9b4eb606a42605c6503c
Matheus Schuh <mschuh@kalray.eu>
2016-07-20 11:01:04 +0200

    Directive to bare compile with device tree inclusion in mOS.

--- misc/mk/common-mppa.mk
+++ misc/mk/common-mppa.mk
@@ -1,3 +1,3 @@
-CONFIG_CFLAGS=$(CONFIG_MPPA_CC_CFLAGS) -iwithprefix include -fno-builtin -DPOK_ARCH_MPPA -D__mos__ $(KIND_CFLAGS) $(GENERIC_FLAGS) -Wall -Wextra -Werror -g -O0 -Wuninitialized -nodefaultlibs -nostdlib
-CFLAGS=$(CONFIG_MPPA_CC_CFLAGS) -iwithprefix include -fno-builtin -DPOK_ARCH_MPPA -D__mos__ $(KIND_CFLAGS) $(GENERIC_FLAGS) -Wall -Wextra -Werror -g -O0 -Wuninitialized -nodefaultlibs -nostdlib
+CONFIG_CFLAGS=$(CONFIG_MPPA_CC_CFLAGS) -iwithprefix include -fno-builtin -DPOK_ARCH_MPPA -D__mos__ -DNO_STD_LIB $(KIND_CFLAGS) $(GENERIC_FLAGS) -Wall -Wextra -Werror -g -O0 -Wuninitialized -nodefaultlibs -nostdlib
+CFLAGS=$(CONFIG_MPPA_CC_CFLAGS) -iwithprefix include -fno-builtin -DPOK_ARCH_MPPA -D__mos__ -DNO_STD_LIB $(KIND_CFLAGS) $(GENERIC_FLAGS) -Wall -Wextra -Werror -g -O0 -Wuninitialized -nodefaultlibs -nostdlib
 LDFLAGS+=$(CONFIG_MPPA_CC_LFLAGS)
